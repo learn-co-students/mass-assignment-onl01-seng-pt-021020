4859010314aae0cc9011d390fabb072e4f9b4b9f
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-18 21:41:41 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
+
+# Person
+#   a longer list of attributes
+#     will assign an arbitrary number of properties on initialization (FAILED - 1)
+#   a different, shorter list of attributes
+#     will also assign an arbitrary number of properties on initialization (FAILED - 2)
+
 class Person
-  #your code here
+ attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+ 
+ def initialize(attributes)
+   attributes.each {|key, value| self.send(("#{key}="), value)}
+ end
 end
